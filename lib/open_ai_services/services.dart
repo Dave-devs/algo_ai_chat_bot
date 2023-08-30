c8644e9c42b34ad081995c593242a90bb638f3ef
Oreoluwa <Odetundeoreoluwadavid@gmail.com>
2023-08-30 01:45:22 +0100

    Refactored all network calls to use http

--- lib/open_ai_services/services.dart
+++ lib/open_ai_services/services.dart
@@ -1,46 +1,41 @@
 import 'dart:convert';
 import 'package:algo_ai_chat_bot/secret/secret_key.dart';
-import 'package:dio/dio.dart';
+import 'package:http/http.dart' as http;
 import 'package:flutter/foundation.dart';
 
 
 class OpenAIServices {
   List<Map<String, String>> messages = [];
-  final dio = Dio();
+  final String chatUrl = "https://api.openai.com/v1/chat/completions";
+  final String dallEUrl = "https://api.openai.com/v1/images/generations";
 
   Future<String> isArtOrTextPrompt(String prompt) async {
     try {
-      Response response;
-
-      response = await dio.post(
-        "https://api.openai.com/v1/chat/completions",
-        options: Options(
-          sendTimeout: const Duration(seconds: 15),
-          receiveTimeout: const Duration(seconds: 15),
-          headers: {
-            "Content-Type": "application/json",
-            "Authorization":
-                "Bearer $openAIApiKey", // Make sure you have openAI_Api_Key defined somewhere
-          },
-        ),
-        data: jsonEncode({
+      final response = await http.post(
+        Uri.parse(chatUrl),
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization":
+          "Bearer $openAIApiKey", // Make sure you have openAI_Api_Key defined somewhere
+        },
+        body: jsonEncode({
           "model": "gpt-3.5-turbo",
           "messages": [
             {
               "role": "user",
               "content":
-                  "Does this message want to generate an AI image, picture, art, photo or anything similar? $prompt. Simply reply with a YES or NO.",
+              "Does this message want to generate an AI image, picture, art, photo or anything similar? $prompt. Simply reply with a YES or NO.",
             }
           ],
         }),
       );
 
       if (kDebugMode) {
-        print(response.data);
+        print(response.body);
       }
 
       if (response.statusCode == 200) {
-        String content = response.data['choices'][0]['message']['content'];
+        String content = jsonDecode(response.body)['choices'][0]['message']['content'];
         content = content.trim();
 
         switch (content.toLowerCase()) {
@@ -66,36 +61,33 @@ class OpenAIServices {
   }
 
   Future<String> isChatGPT(String prompt) async {
+
+    messages.add({'role': 'user', 'content': prompt});
+
     try {
-      Response response;
-      response = await dio.post(
-        "https://api.openai.com/v1/chat/completions",
-        options: Options(
-          headers: {
-            "Content-Type": "application/json",
-            "Authorization": "Bearer $openAIApiKey", // Make sure you have openAIApiKey defined somewhere
-          },
-        ),
-        data: jsonEncode({
+      final response = await http.post(
+          Uri.parse(chatUrl),
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": "Bearer $openAIApiKey", // Make sure you have openAIApiKey defined somewhere
+        },
+        body: jsonEncode({
           "model": "gpt-3.5-turbo",
           "messages": messages,
-        }),
+        })
       );
 
       if (response.statusCode == 200) {
-        String content = response.data['choices'][0]['message']['content'];
+        String content = jsonDecode(response.body)['choices'][0]['message']['content'];
         content = content.trim();
 
         messages.add({
           "role": "assistant",
           "content": content,
         });
-
         return content;
       }
-
       return 'An internal error occurred';
-      
     } catch (e) {
       return e.toString();
     }
@@ -105,33 +97,26 @@ class OpenAIServices {
     messages.add({"role": "user", "content": prompt});
 
     try {
-      Response response;
-      response = await dio.post(
-        "https://api.openai.com/v1/images/generations",
-
-        options: Options(
-          headers: {
-            "Content-Type": "application/json",
-            "Authorization": "Bearer $openAIApiKey", // Make sure you have openAI_Api_Key defined somewhere
-          },
-        ),
-        data: jsonEncode({"prompt": prompt, "n": 1, "size": "1024x1024"}),
+      final response = await http.post(
+        Uri.parse(dallEUrl),
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": "Bearer $openAIApiKey", // Make sure you have openAI_Api_Key defined somewhere
+        },
+        body: jsonEncode({"prompt": prompt, "n": 1, "size": "1024x1024"}),
       );
 
       if (response.statusCode == 200) {
-        String imageUrl = response.data['data'][0]['url'];
+        String imageUrl = jsonDecode(response.body)['data'][0]['url'];
         imageUrl = imageUrl.trim();
 
         messages.add({
           "role": "assistant",
           "content": imageUrl,
         });
-
         return imageUrl;
-      } 
-      
+      }
       return 'An internal error occurred';
-
     } catch (e) {
       return e.toString();
     }
